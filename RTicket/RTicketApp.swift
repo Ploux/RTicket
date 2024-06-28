82a744234a3a3d15557f4f4abdac1deb42ae87f8
Ploux <peter@humo.golf>
2024-06-28 16:55:31 -0500

    Initial Commit

---
+++ RTicket/RTicketApp.swift
@@ -0,0 +1,17 @@
+//
+//  RTicketApp.swift
+//  RTicket
+//
+//  Created by Peter Loux on 6/28/24.
+//
+
+import SwiftUI
+
+@main
+struct RTicketApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
