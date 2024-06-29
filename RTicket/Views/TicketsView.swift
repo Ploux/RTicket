74aee77bfc0697c9e73718a4b96730b3e0454f68
Ploux <peter@humo.golf>
2024-06-28 20:51:00 -0500

    add subscribe and unsubscribe functions to TicketsView

--- RTicket/Views/TicketsView.swift
+++ RTicket/Views/TicketsView.swift
@@ -17,9 +17,60 @@ struct TicketsView: View {
     let username: String
     let product: String
     
+    @State private var busy = false
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            VStack {
+                List {
+                    ForEach(tickets) { ticket in
+                        Text(ticket.title)
+                    }
+                }
+                Spacer()
+            }
+            .padding()
+            if busy {
+                ProgressView()
+            }
+        }
     }
+    
+    private func subscribe() {
+        let lastYear = Date(timeIntervalSinceReferenceDate: Date().timeIntervalSinceReferenceDate.rounded() - (60 * 60 * 24 * 365))
+        let subscriptions = realm.subscriptions
+        if subscriptions.first(named: product) == nil {
+            DispatchQueue.main.async {
+                self.busy = true // Access and mutate 'busy' on the main actor
+            }
+            subscriptions.update {
+                subscriptions.append(QuerySubscription<Ticket>(name: product) { ticket in
+                    return ticket.product == product && (ticket.status != .complete || ticket.created > lastYear)
+                })
+            } onComplete: { error in
+                DispatchQueue.main.async {
+                    if let error = error {
+                        print("Failed to subscribe for \(product): \(error.localizedDescription)")
+                    }
+                    self.busy = false // Access and mutate 'busy' on the main actor
+                }
+            }
+        }
+    }
+    
+    private func unsubscribe() {
+        let subscriptions = realm.subscriptions
+        subscriptions.update {
+            subscriptions.remove(named: product)
+        } onComplete: { error in
+            DispatchQueue.main.async {
+                if let error = error {
+                    print("Failed to unsubscribe for \(product): \(error.localizedDescription)")
+                }
+            }
+        }
+    }
+    
 }
 
 #Preview {
